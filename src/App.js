db970e43e486fc33f0f8d81499a0187cbbf9384b
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-10-03 01:12:49 +0200

    protect pages from visit unauth user

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import { Switch, Route } from 'react-router-dom';
+import { useContext } from "react";
 
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import { Redirect, Route, Switch } from "react-router-dom";
+
+import AuthContext from "./store/auth-context";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
 
 function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <HomePage />
-        </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
-        </Route>
-      </Switch>
-    </Layout>
-  );
+	const context = useContext(AuthContext);
+
+	return (
+		<Layout>
+			<Switch>
+				<Route path="/" exact>
+					<HomePage />
+				</Route>
+				{!context.isLoggedIn && (
+					<Route path="/auth">
+						<AuthPage />
+					</Route>
+				)}
+				<Route path="/profile">
+					{context.isLoggedIn && <UserProfile />}
+					{!context.isLoggedIn && <Redirect to="/auth" />}
+				</Route>
+				<Route path="*">
+					<Redirect to="/" />
+				</Route>
+			</Switch>
+		</Layout>
+	);
 }
 
 export default App;
