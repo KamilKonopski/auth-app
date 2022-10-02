024224c86dca1103e448d12be33c0436f8ae3cdd
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2022-10-03 00:54:56 +0200

    use token to protect resources

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,50 @@
-import classes from './ProfileForm.module.css';
+import { useContext, useRef } from "react";
 
-const ProfileForm = () => {
-  return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
-  );
+import AuthContext from "../../store/auth-context";
+
+import classes from "./ProfileForm.module.css";
+
+function ProfileForm() {
+	const newPasswordInputRef = useRef();
+	const context = useContext(AuthContext);
+
+	function submitHandler(event) {
+		event.preventDefault();
+
+		const enteredNewPassword = newPasswordInputRef.current.value;
+
+		fetch(
+			"https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDOc-dxGRJLjd3g--ZdPU3QKVIzm2l1nYs",
+			{
+				method: "POST",
+				body: JSON.stringify({
+					idToken: context.token,
+					password: enteredNewPassword,
+					returnSecureToken: false,
+				}),
+				headers: {
+					"Content-type": "application/json",
+				},
+			}
+		).then((response) => {});
+	}
+
+	return (
+		<form className={classes.form} onSubmit={submitHandler}>
+			<div className={classes.control}>
+				<label htmlFor="new-password">New Password</label>
+				<input
+					type="password"
+					ref={newPasswordInputRef}
+					id="new-password"
+					minLength="7"
+				/>
+			</div>
+			<div className={classes.action}>
+				<button>Change Password</button>
+			</div>
+		</form>
+	);
 }
 
 export default ProfileForm;
